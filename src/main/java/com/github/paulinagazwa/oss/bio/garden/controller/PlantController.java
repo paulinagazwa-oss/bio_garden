70df75d1ba8c779a22079f647e79237d5b26636a
Paulina Gązwa <paulina.gazwa@gmail.com>
2026-03-29 14:42:59 +0200

    Controllers to OpenApi, template to PlanetService
    Some Doku in README.md

--- src/main/java/com/github/paulinagazwa/oss/bio/garden/controller/PlantController.java
+++ src/main/java/com/github/paulinagazwa/oss/bio/garden/controller/PlantController.java
@@ -1,9 +1,16 @@
 package com.github.paulinagazwa.oss.bio.garden.controller;
 
-import com.github.paulinagazwa.oss.bio.garden.entity.Plant;
+import com.github.paulinagazwa.oss.bio.garden.model.Plant;
+import com.github.paulinagazwa.oss.bio.garden.model.PlantCreateRequest;
+import com.github.paulinagazwa.oss.bio.garden.model.PlantUpdateRequest;
 import com.github.paulinagazwa.oss.bio.garden.service.PlantService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,9 +21,33 @@ public class PlantController {
 	@Autowired
 	private PlantService plantService;
 
-	@GetMapping ("/plants")
+	@GetMapping("/api/v1/plants")
 	public List<Plant> getAllPlants() {
 
 		return plantService.findAllPlants();
+	}
+
+	@GetMapping("/api/v1/plants/{id}")
+	public Plant getPlantById(@PathVariable Long id) {
+
+		return plantService.findPlantById(id);
+	}
+
+	@PostMapping("/api/v1/plants")
+	public Plant createPlant(@Valid PlantCreateRequest plantCreateRequest) {
+
+		return plantService.createPlant(plantCreateRequest);
+	}
+
+	@PutMapping("/api/v1/plants/{id}")
+	public Plant updatePlant(@Valid PlantUpdateRequest plantUpdateRequest) {
+
+		return plantService.updatePlant(plantUpdateRequest);
+	}
+
+	@DeleteMapping("/api/v1/plants/{id}")
+	public void deletePlant(@PathVariable Long id) {
+
+		plantService.deletePlant(id);
 	}
 }
--- src/main/java/com/github/paulinagazwa/oss/bio/garden/service/PlantService.java
+++ src/main/java/com/github/paulinagazwa/oss/bio/garden/service/PlantService.java
@@ -1,6 +1,8 @@
 package com.github.paulinagazwa.oss.bio.garden.service;
 
-import com.github.paulinagazwa.oss.bio.garden.entity.Plant;
+import com.github.paulinagazwa.oss.bio.garden.model.Plant;
+import com.github.paulinagazwa.oss.bio.garden.model.PlantCreateRequest;
+import com.github.paulinagazwa.oss.bio.garden.model.PlantUpdateRequest;
 
 import java.util.List;
 
@@ -8,4 +10,11 @@ public interface PlantService {
 
 	List<Plant> findAllPlants();
 
+	Plant findPlantById(Long id);
+
+	Plant createPlant(PlantCreateRequest plantCreateRequest);
+
+	Plant updatePlant(PlantUpdateRequest plantUpdateRequest);
+
+	void deletePlant(Long id);
 }
